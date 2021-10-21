557f78f0b2200e033b7972158ec37c8981e478e6
Adam Jędrzejewski <59581316+adamjedrzejewski@users.noreply.github.com>
2021-10-21 21:34:15 +0200

    added db context

---
+++ Server/Models/MainDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stocks.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stocks.Server.Models
+{
+    public class MainDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public MainDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(builder =>
+            {
+                builder.ToTable("users");
+                builder.HasKey(e => e.UserId);
+                builder.Property(e => e.Username).IsRequired().HasMaxLength(50);
+                builder.Property(e => e.Password).IsRequired().HasMaxLength(100);
+            });
+
+        }
+    }
+}
--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,10 +1,13 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Stocks.Server.Models;
 using System.Linq;
 
 namespace Stocks.Server
@@ -22,9 +25,17 @@ namespace Stocks.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MainDbContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("default"));
+            });
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +58,8 @@ namespace Stocks.Server
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
