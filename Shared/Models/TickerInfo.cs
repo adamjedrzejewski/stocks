42156bcdcb2b8bd859d2c755c140b70cea3ed4fd
Adam Jędrzejewski <59581316+adamjedrzejewski@users.noreply.github.com>
2021-10-22 20:27:28 +0200

    sample ohlc page

---
+++ Shared/Models/TickerInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Stocks.Shared.Models
+{
+    public class TickerInfo
+    {
+        [JsonPropertyName("Time Series (Daily)")]
+        public Dictionary<DateTime, TickerDataPoint> TimeSeries { get; set; }
+    }
+}
