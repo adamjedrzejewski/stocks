f5e7d5c9b9719b0f9b58b7d4a8a06e6ae54ff3cc
Adam Jędrzejewski <59581316+adamjedrzejewski@users.noreply.github.com>
2021-10-23 18:46:30 +0200

    watchlist stubs

---
+++ Server/Controllers/WatchlistController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stocks.Client.Pages;
+using Stocks.Server.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stocks.Server.Controllers
+{
+    [Route("api/watchlist")]
+    [ApiController]
+    public class WatchlistController : ControllerBase
+    {
+        private readonly IDatabaseService _databaseService;
+
+        public WatchlistController(IDatabaseService databaseService)
+        {
+            this._databaseService = databaseService;
+        }
+
+        [HttpGet("{userID}")]
+        public async Task<ActionResult<Ticker[]>> GetUserWatchList(int userId)
+        {
+            return new Ticker[0];
+        }
+
+    }
+}
---
+++ Shared/Models/WatchTicker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stocks.Shared.Models
+{
+    class WatchTicker
+    {
+        public string TickerName { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
